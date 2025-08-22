3ba74a1c90273bd726b5dd2d7989e6c6d19d7b33
mindelixai <mindelixai@gmail.com>
2025-08-23 00:14:19 +0200

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // IMPORTANT: set to your repo name so assets load on GitHub Pages
-  base: '/Art-of-the-Prompt/',
-})
